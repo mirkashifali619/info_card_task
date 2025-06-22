6b2c1f2ac7f12019d13320e45a4e57ad38365eee
mirkashifali619 <mirkashifali619@gmail.com>
2025-06-22 23:58:31 +0530

    Commit_22_06_2025

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function toggleExtra() {
-    const info = document.getElementById('extraInfo');
-    const btn = document.querySelector('.show_hobbies');
+    const info = document.getElementById('extraInfo'); // selects hidden information 
+    const btn = document.querySelector('.show_hobbies'); // selects the button that was clicked
     if (info.style.display === 'block') {
-        info.style.display = 'none';
-        btn.textContent = 'Show Hobbies';
+        info.style.display = 'none'; // hiding content if visible
+        btn.textContent = 'Show Hobbies'; // changing button label
     }else {
-        info.style.display = 'block';
-        btn.textContent = 'Hide Hobbies';
+        info.style.display = 'block'; // if its currently visible, showing it.
+        btn.textContent = 'Hide Hobbies'; // updating the labe;
     }
 }
-function changeBg(sel) {
+function changeBg(sel) { // sell refers to select statement 
     document.getElementById('card').style.background = sel.value;
 }
